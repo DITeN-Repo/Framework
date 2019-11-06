cc24991bbb45474384037516289c655596933976
Arash Rahimian <DITeN-Repo@outlook.com>
2019-11-06 16:40:58 +0330

    Test

---
+++ Libraries/Foundation/Diten.Standard/Object.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//Test vhjkgykj
+namespace Diten
+{
+	/// <summary>
+	/// Representing the standard object in DITeN Framework that all other objects will be inherited from this object.
+	/// <seealso cref="Collections.Generic.DeepObject{TObject}"/>
+	/// </summary>
+	public class Object: Collections.Generic.DeepObject<Object> {}
+}
